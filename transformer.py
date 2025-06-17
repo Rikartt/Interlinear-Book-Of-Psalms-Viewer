957303ad9d6fc99c80e2dc78292c22f2f6369860
Rikard Blennhem <49799736+Rikartt@users.noreply.github.com>
2025-06-17 19:13:13 +0200

    changed the transformer to include strong's dictionary info

--- transformer.py
+++ transformer.py
@@ -4,6 +4,10 @@ f = open('hebrew/hebrew.json', encoding='utf-8')
 data = json.load(f)
 psalms_hebrew = (data['Psalms'])
 f.close()
+with open('strongs-master\strongs-master\strongs-hebrew-dictionary.json', 'r', encoding='utf-8') as f:
+    strongs = json.load(f)
+    print()
+    f.close()
 def openEnglish(Chapter):
     f = open(f'BibleJSON-main/BibleJSON-main/JSON/Psalms/{Chapter}.json', encoding='utf-8')
     #print(f'BibleJSON-main/BibleJSON-main/JSON/Psalms/{Chapter}.json')
@@ -21,6 +25,32 @@ def openEnglish(Chapter):
 #Get hebrew bible
 f = open('D:\Things I need to Pyth\Websites\BOOK OF PSALMS\hebrew\hebrew.json', 'r', encoding='utf-8')
 heb = json.load(f)
+def transformhebrew(l):
+    returnlis = {}
+    for book_name, chapters in l.items():
+        cs = []
+        for chapter_index, chapter in enumerate(chapters):
+            c = []
+            for verse_index, verse in enumerate(chapter):
+                words = []
+                for word_index, word_data in enumerate(verse):
+                    word, strong, morph = word_data
+                    words.append({
+                        'word': word.split("/"),
+                        'strong': strong.split("/"),
+                        'morph': morph.split("/"),
+                        'lemma': [strongs.get(x, {}).get('lemma') for x in strong.split("/")],
+                        'xlit': [strongs.get(x, {}).get('xlit') for x in strong.split("/")],
+                        'pron': [strongs.get(x, {}).get('pron') for x in strong.split("/")],
+                        'derivation': [strongs.get(x, {}).get('derivation') for x in strong.split("/")],
+                        'strongs_def': [strongs.get(x, {}).get('strongs_def') for x in strong.split("/")],
+                        'gloss': [strongs.get(x, {}).get('kjv_def') for x in strong.split("/")]
+                    })
+                c.append(words)
+            cs.append(c)
+        returnlis[book_name] = cs
+    return returnlis
+heb = transformhebrew(heb)
 f.close()
 Psalms = {"chapters": []}
 for i in range(1, 151):
